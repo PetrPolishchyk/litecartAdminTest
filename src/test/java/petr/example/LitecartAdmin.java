2e06c0a3a5480caf46076bca63a6b0f60a4f1557
PolishchukPeter <Polishchyk87const@gmail.com>
2022-05-07 17:19:44 +0300

    success first start

--- src/test/java/petr/example/LitecartAdmin.java
+++ src/test/java/petr/example/LitecartAdmin.java
@@ -1,14 +1,21 @@
 package petr.example;
 
 import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
 
 @Test
 public class LitecartAdmin {
     @Test
     public void testName (){
         Selenide.open("http://localhost/litecart/admin/login.php?redirect_url=%2Flitecart%2Fadmin%2F");
-        Selenide.Wait();
+        $(By.name("username")).setValue("admin");
+        $(By.name("password")).setValue("admin");
+        $(By.xpath("//div[@class='footer']/button")).click();
+        $(By.xpath("//span[text()='Appearance']")).click();
+        $(By.xpath("//i[@class='fa fa-sign-out']")).click();
         System.out.println("Success");
     }
 }
