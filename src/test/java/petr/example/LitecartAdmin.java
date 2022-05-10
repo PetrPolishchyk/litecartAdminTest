48869828abe0363f05b89a3ccbd10fc8015bbe5f
PolishchukPeter <Polishchyk87const@gmail.com>
2022-05-10 20:29:43 +0300

    plus string x

--- src/test/java/petr/example/LitecartAdmin.java
+++ src/test/java/petr/example/LitecartAdmin.java
@@ -1,8 +1,10 @@
 package petr.example;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.Condition;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import static com.codeborne.selenide.Condition.*;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -10,6 +12,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class LitecartAdmin {
     @Test
     public void testName (){
+
+        Configuration.timeout=6000;
         Selenide.open("http://localhost/litecart/admin/login.php?redirect_url=%2Flitecart%2Fadmin%2F");
         $(By.name("username")).setValue("admin");
         $(By.name("password")).setValue("admin");
@@ -17,5 +21,10 @@ public class LitecartAdmin {
         $(By.xpath("//span[text()='Appearance']")).click();
         $(By.xpath("//i[@class='fa fa-sign-out']")).click();
         System.out.println("Success");
+
+        //$("#submit").shouldBe(Condition.enabled).click();
+        String x = String.valueOf($(By.xpath("//div[@class='footer']/button")).getValue());
+        System.out.println(x);
+        //SelenideElement input = $("#input").should(visible);
     }
 }
